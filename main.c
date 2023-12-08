1449a833e12abf5fa54ca5fc0fd57beadb6e6f3e
minjiwkim <144991060+minjiwkim@users.noreply.github.com>
2023-12-08 13:48:07 +0900

    2

--- main.c
+++ main.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Book
+{
+       int number;
+       char title[10];
+}
+
 int main(int argc, char *argv[])
 {
-    char *pc = NULL;
-    int i = 0;
+    struct Book *p;
     
-    pc = (char*)malloc(100*sizeof(char));
-    if(pc == NULL)
+    p = (struct Book*)malloc(2*sizeof(struct Book));
+    
+    
+    if(p == NULL)
     {
         printf("메모리 할당 오류\n");
-        exit(1);
+        return;
     }
           
-    for (i=0;i<26;i++)
-    {
-        pc[i] = 'a' + i;
-    }
+    p->number = 1;
+    strcpy(p->title, "C Programming");
     
-    pc[i] = 0;
-    printf("%s\n", pc);
+    (p+1)->number = 2;
+    strcpy((p+1)->title, "Electronics");
     
-    free(pc);
-  
+    free(p);
+    
   system("PAUSE");	
   return 0;
 }
